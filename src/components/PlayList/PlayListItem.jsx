7a075c4801a87b5a1b316a62afcd76a7410adfe0
jyc-coder <jyc4648@gmail.com>
2022-09-14 01:54:20 +0900

    #21 - play list 재생시간 표시 구현
    *  `musicPlayerReducer`에서 데이터를 가져오므로 이제 필요없는 `data.js`는 삭제함
    
    * `PlyaListItem`에서 prop으로 받아온 `item`에서 `music`파일을 새로 만든 `Audio`객체에서 `onloadaedmetadata`메서드를 통해서 "분"과 "초"에 해당하는 데이터를 정의한뒤 이를 가공해주는 `getDuration`을 구현함
    
    * `getDuration`을 통해 변경된 `duration`의 값을 span에 추가함

--- src/components/PlayList/PlayListItem.jsx
+++ src/components/PlayList/PlayListItem.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import { useSelector } from "react-redux";
 
+const getDuration = (music) => {
+  return new Promise((resolve) => {
+    const audio = new Audio();
+    audio.onloadedmetadata = () => {
+      const minutes = `0${parseInt(audio.duration / 60, 10)}`;
+      const seconds = `0${parseInt(audio.duration % 60)}`;
+      resolve(`${minutes}:${seconds.slice(-2)}`);
+    };
+    audio.src = music;
+  });
+};
+
 const PlayListItem = ({ item, index }) => {
   const currentIndex = useSelector((state) => state.currentIndex);
+  const [duration, setduration] = useState("00:00");
+  useEffect(() => {
+    async function getTime() {
+      const durationTime = await getDuration(item.music);
+      setduration(durationTime);
+    }
+    getTime();
+  }, [item.music]);
+
   return (
     <>
       <div className={classNames("row", { playing: currentIndex === index })}>
@@ -15,7 +36,7 @@ const PlayListItem = ({ item, index }) => {
           playing: currentIndex === index,
         })}
       >
-        00:00
+        {duration}
       </span>
     </>
   );
